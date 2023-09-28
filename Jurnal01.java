fcb7e613ffee97f0edf6dab2eeddb7c960d9957d
Hermulia <hermuliaanjelita@gmail.com>
2023-09-29 01:01:45 +0700

    AAAAA akhirnya aku bisa ibu cahyanaaa, aku bisaaaa

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,7 +1,11 @@
 public class Jurnal01{
     public static void main(String[] args) {
-        System.out.print("Hello" + " ");
-        System.out.println("World");
+        System.out.print("Nama: " + " ");
+        System.out.println("Anjelita Hermulia Putri");
+        System.out.print("NIM: " + " ");
+        System.out.println("607062330044");
+        System.out.print("Kelas: " + " ");
+        System.out.println("D3IF-47-01");
        } 
     
 }
